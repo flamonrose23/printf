fa8cbd037f2e1ebf05ef20ec4bfafd193b7f0440
flamonrose23 <fatinemar20@icloud.com>
2023-04-27 16:39:00 -0700

    Task 2 radi nchoufo

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplying two integers.
+ * @x: first integer
+ * @y: second integer
+ * Return: multiplication
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
