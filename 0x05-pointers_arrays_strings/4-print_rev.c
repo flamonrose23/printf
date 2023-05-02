debf90606674ab02a2aa9d7ca6ba5a9cfc93b3de
flamonrose23 <fatinemar20@icloud.com>
2023-05-02 15:24:40 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * followed by a new line
+ * @s: string
+ * Return: always 0
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int x;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (x = longi; x > 0; x--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
