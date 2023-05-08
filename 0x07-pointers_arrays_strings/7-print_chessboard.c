2af7f7eba973f5f4d3023677c2075bf9a9af983e
flamonrose23 <fatinemar20@icloud.com>
2023-05-08 16:11:39 -0700

    task 7 : inchallah khir

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - unction that prints the chessboard
+ * @a: array
+ * Return: always 0 (success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
+		_putchar('\n');
+	}
+}
