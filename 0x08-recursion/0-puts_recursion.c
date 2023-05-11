9de32f2c47538f025ac221152b2373d77e6c0a34
flamonrose23 <fatinemar20@icloud.com>
2023-05-11 10:58:58 -0700

    Task 0 : dayza inchallah

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string
- * followed by mew line
+ * _puts_recursion - function like puts();
  * @s: input value
- * Return: always 0
+ * Return: always 0 (success)
  */
 
 void _puts_recursion(char *s)
