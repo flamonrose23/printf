bce859bc231c206e1cda3bda751c7413a8cb53bc
flamonrose23 <fatinemar20@icloud.com>
2023-05-11 10:48:03 -0700

    task0 njereb wach ydouz

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * followed by mew line
+ * @s: input value
+ * Return: always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
