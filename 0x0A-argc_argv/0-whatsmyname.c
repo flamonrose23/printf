342cc18539c2cf56f73f2703eb3ef368cec827c0
flamonrose23 <fatinemar20@icloud.com>
2023-05-17 15:23:08 -0700

    Task 0 updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,9 @@
  * Return: always 0 (success)
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char const *argv[])
 {
-	printf("%s\n", *argv);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
