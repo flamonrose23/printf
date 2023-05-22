f650caf6bc610a8664a1084135cbab9fece7023e
flamonrose23 <fatinemar20@icloud.com>
2023-05-22 16:07:02 -0700

    Task 0 again

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: age of a dog
  * @owner: owner of a dog
  *
- * Return: always 0
+ * Description: casual desciption
  */
 
 struct dog
@@ -22,10 +22,11 @@ struct dog
  */
 
 typedef struct dog dog_t
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
